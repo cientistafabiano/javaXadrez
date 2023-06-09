94b65cfd5c04c731e13f84d13a6dcf421653dd34
cientistafabiano <fabiano.dev.full@gmail.com>
2023-06-09 17:44:36 -0300

    Class primary: Position

---
+++ src/boardGame/Position.java
@@ -0,0 +1,29 @@
+package boardGame;
+
+public class Position {
+	private Integer row;
+	private Integer column;
+	//constructor
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+	//getters and setters
+	public Integer getRow() {
+		return row;
+	}
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+	public Integer getColumn() {
+		return column;
+	}
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+}
